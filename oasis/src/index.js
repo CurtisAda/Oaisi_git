5ee524d955038582cbbc951a2558928e358b7c9b
CurtisAda <164827054+CurtisAda@users.noreply.github.com>
2025-07-09 12:47:28 -0500

    small fix

--- oasis/src/index.js
+++ oasis/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter  basename="/Oaisi_git">
     <App />
   </BrowserRouter>
 );
